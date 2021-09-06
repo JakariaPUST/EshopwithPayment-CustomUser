c029f18d92e32eed3e54b2810995c5517ce576ea
JakariaPUST <jakaria.pust@gmail.com>
2021-09-06 19:39:06 +0600

    Dynamic links added, Details product view included

--- app_shop/urls.py
+++ app_shop/urls.py
@@ -3,5 +3,6 @@ from app_shop import views
 app_name = 'app_shop'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('product/<pk>/', views.ProductDetails.as_view(), name='product_details')
 ]
--- app_shop/views.py
+++ app_shop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from app_shop.models import Product
 # Create your views here.
 
 
 class Home(ListView):
     model = Product
-    template_name = 'App_Shop/product_list.html'
+    template_name = 'app_shop/home.html'
+
+
+class ProductDetails(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'app_shop/product_details.html'
